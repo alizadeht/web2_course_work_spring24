713973cbbafab49a32b28cca1e3171919fcbb08a
alizadeht <turalalizadeh4@gmail.com>
2024-02-19 15:00:09 +0400

    Week 4 - CRUD app

---
+++ first-spring-app/src/main/java/az/edu/ada/wm2/firstspringapp/FirstSpringAppApplication.java
@@ -0,0 +1,20 @@
+package az.edu.ada.wm2.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+@RequestMapping
+
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);}
+	}
+
+
+	@GetMapping("/")
+	public String sayHello(){
+		return "Hi there";
+	}
